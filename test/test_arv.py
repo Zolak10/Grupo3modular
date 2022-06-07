b29981646464ee9c62bdc9813bc58031bf658a37
João Koehler <62446005+Zolak10@users.noreply.github.com>
2022-06-07 18:35:13 -0300

    Update test_arv.py

--- test/test_arv.py
+++ test/test_arv.py
@@ -6,6 +6,7 @@ import arv
 
 def test_verificaPrefixo():
     assert arv.verificaPrefixo('aaaaaaaaa') == False
-    
+'''   
 def test_wordInArq():
     assert arv.wordInArq('banana','frutas.txt') == True
+'''
