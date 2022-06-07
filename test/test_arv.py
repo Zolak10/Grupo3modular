80378f6265ff2fc185a65a5975c6cae8f7f11f19
Zolak10 <jubakoehler@gmail.com>
2022-06-06 21:31:38 -0300

    testes

--- test/test_arv.py
+++ test/test_arv.py
@@ -6,7 +6,5 @@ import arv
 
 def test_wordInarq():
     assert arv.wordInArq('frutas.txt','banana')
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
+
 
