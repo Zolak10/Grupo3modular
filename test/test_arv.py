e1c0f87cb66f5c935ab8e2d278a48d05c8045ab1
João Koehler <62446005+Zolak10@users.noreply.github.com>
2022-06-07 17:48:26 -0300

    Update test_arv.py

--- test/test_arv.py
+++ test/test_arv.py
@@ -5,7 +5,7 @@ sys.path.append(path_to_module)
 import arv
 
 def test_wordInarq():
-    assert arv.wordInArq('frutas.txt','banana') == True
+    assert arv.wordInArq('banana','frutas.txt') == True
 
 def test_verificaPrefixo():
-    assert verificaPrefixo('aaaaaaaaa') == False
+    assert arv.verificaPrefixo('aaaaaaaaa') == False
